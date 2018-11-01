d6be37b80f9e0bf9c268b32d614f814c5af8579e
徐佳 <jia.xu@tiejin.me>
2018-11-01 14:44:28 +0800

    添加antd UI框架

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Router from './router/index';
 import './assets/style/index.css'
+import 'antd/dist/antd.css';
 
 ReactDOM.render(
   <Router/>,
--- my-app/src/pages/home/index.js
+++ my-app/src/pages/home/index.js
@@ -1,27 +1,57 @@
 import React from 'react';
+import { Input, message, Button } from 'antd';
+
 import './index.scss'
 
 export default class Home extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            phoneNo: '',
+            code: ''
+        };
+
+        this.handleInputNo = this.handleInputNo.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleInputCode = this.handleInputCode.bind(this);
     }
-    state = {
-        alertStatus: false, //弹框状态
-        alertTip: '', //提示弹框文字
-        phoneNo: ''
+    handleInputNo(event) {
+        this.setState({
+            phoneNo: event.target.value
+        })
     }
-    handleInput(type, event) {
-        let value = event.target.value
-        console.log(value)
-        this.phoneNo = value
-        console.log(this.phoneNo)
+    handleInputCode(event) {
+        this.setState({
+            code: event.target.value
+        })
     }
-    submit() {
-        const phoneNo = this.state.phoneNo
+    handleSubmit(event) {
+        let phoneNo = this.state.phoneNo,
+            code = this.state.code,
+            phoneReg = /^(0|86|17951)?(13[0-9]|15[012356789]|166|17[3678]|18[0-9]|14[57])[0-9]{8}$/,
+            codeReg = /^\d{6}$/
         if(!phoneNo) {
-            console.log('dianhuaweikong')
+            message.info('电话号码不能为空');
+            return
         }
+        if(!phoneReg.test(phoneNo)) {
+            message.info('请输入正确格式的电话号码')
+            return
+        }
+        if(!code) {
+            message.info('验证码不能为空');
+            return
+        }
+        if(!codeReg.test(code)) {
+            message.info('请输入正确的验证码')
+            return
+        }
+        alert('登录成功')
+        event.preventDefault();
     }
+     info (){
+        message.info('电话号码不能为空');
+      };
     render() {
         return (
             // <div>
@@ -38,14 +68,14 @@ export default class Home extends React.Component {
                     <div className='login-title'>登录</div>
                     <div className='login-info'>
                         <div className='phone'>
-                            <input type='text' placeholder='请输入手机号' maxLength='11' />
+                         <Input placeholder='请输入手机号' maxLength='11' value={this.state.phoneNo} onChange={this.handleInputNo} />
                         </div>
                         <div className='code'>
-                            <input type='text' placeholder='请输入验证码' maxLength='6' />
-                            <span>获取验证码</span>
-                        </div>
+                          <Input placeholder='请输入验证码' maxLength='6' value={this.state.code} onChange={this.handleInputCode} />
+                          <span>获取验证码</span>
+                        </div> 
                     </div>
-                    <div className='login-btn'>登录</div>
+                    <Button className='login-btn' type="primary" block onClick={this.handleSubmit}>登录</Button>
                 </div>
             </div>
         )
